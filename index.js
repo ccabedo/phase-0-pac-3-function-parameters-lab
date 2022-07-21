5e6936765b28db9eec80ea2a3f91d4ec18e3115c
Colleen Cabedo <cmcabedo@gmail.com>
2022-07-21 15:16:17 -0400

    commit tree f3479b5ff8c51d5417e6b7da54a6001dab1abb83

---
+++ index.js
@@ -0,0 +1,9 @@
+const introduction = "Hi, my name is"
+let name = (Aki)
+let name = (Samip)
+
+function greeting (introduction, name) {
+    console.log(introduction + name);
+    return introduction;
+}
+
